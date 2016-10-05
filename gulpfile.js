f5880fcccdf744ab74c77563481814fd8f7e9300
Sebastian Serna <jsebastianserna@gmail.com>
2016-10-04 21:23:48 -0500

    Gulp file included

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+//-----------------------------------------------------
+// SWAN UI
+// by Sebastian Serna
+// 2016
+//-----------------------------------------------------
+
+'use strict';
+
+var gulp = require( 'gulp' ),
+	plumber = require( 'gulp-plumber'),
+	sass = require( 'gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer'),
+  minifyCSS = require('gulp-minify-css'),
+  rename = require('gulp-rename'),
+	uglify = require( 'gulp-uglify'),
+	concat = require( 'gulp-concat'),
+	sassdoc = require('sassdoc'),
+  browserSync = require('browser-sync');
+
+
+//-----------------------------------------------------
+// Sass compiler variables
+//-----------------------------------------------------
+
+var input = 'assets/styles/sass/**/*.scss';
+var output = 'assets/styles/css/';
+var projectroot = './*.html';
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+//-----------------------------------------------------
+// Sass compiler task
+//-----------------------------------------------------
+
+gulp.task ('sass' , function() {
+     return gulp
+     	.src(input)
+      .pipe(plumber())
+	    .pipe(sass(sassOptions).on('error', sass.logError))
+    	.pipe(autoprefixer())
+	    .pipe(gulp.dest(output))
+      .pipe(minifyCSS())
+      .pipe(rename('styles.min.css'))
+      .pipe(gulp.dest(output))
+      .pipe(browserSync.stream());
+});
+
+
+//-----------------------------------------------------
+// Browser Sync task (static server)
+//-----------------------------------------------------
+
+gulp.task ('browser-sync' , function() {
+    browserSync.init([ output, projectroot ], {
+        server: { 
+          baseDir: "./"
+        }
+    });
+});
+
+
+//-----------------------------------------------------
+// Sass Docs task
+//-----------------------------------------------------
+
+gulp.task('sassdoc', function () {
+  return gulp
+    .src(input)
+    .pipe(sassdoc())
+    .resume();
+});
+
+//-----------------------------------------------------
+// Scripts merge task
+//-----------------------------------------------------
+
+gulp.task ('mergejs' , function() {
+	gulp.src ('assets/scripts/all/*.js')
+	  .pipe(plumber())
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/scripts/'));
+});
+
+//-----------------------------------------------------
+// Production package tasks
+//-----------------------------------------------------
+
+gulp.task('prod', ['sassdoc'], function () {
+  return gulp
+    .src(input)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
+});
+
+//-----------------------------------------------------
+// Watch tasks
+//-----------------------------------------------------
+
+gulp.task('watch', ['sass' , 'browser-sync'], function() {
+    gulp.watch(input, ['sass']);
+});
+
+
