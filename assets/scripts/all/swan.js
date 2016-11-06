0bc18454999c3e5d51fbecf87c11c5d4d4ac89d5
Sebastian Serna <jsebastianserna@gmail.com>
2016-11-05 20:08:09 -0500

    Provisional components with vanilla JavaScript

--- assets/scripts/all/swan.js
+++ assets/scripts/all/swan.js
@@ -1,31 +1,79 @@
-// Menu mobile
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Swan UI 0.1.0
+// Provisional Vanilla Javascript Code
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-$(document).ready(function(){
-  $('.menu-mobile-open,.menu-mobile-fade-screen').on('click touchstart',function (e) {
-    $('.menu-mobile,.menu-mobile-fade-screen').toggleClass('is-visible');
+// --------------------------------------------------------------
+// MOBILE MENU
+// --------------------------------------------------------------
+// This solution is temporal
+
+var menu = document.getElementById('menu-main');
+var menuShow = document.getElementById('btnMenu');
+var menuFadeBg = document.getElementById('fadeScreen');
+
+function showMobileBtn() {
+    if (window.innerWidth <= 768) {
+        menu.className = "menu-mobile";
+    } else {
+        menu.className = "menu";
+    }
+ }
+
+showMobileBtn();
+window.onresize = function() {
+  showMobileBtn();
+}
+
+// On click Events ::::::::::::::::::
+
+menuShow.onclick = function(event){
+     menu.className += " is-visible";
+     menuFadeBg.className += " is-visible";
+     event.preventDefault();
+}
+// Shame but works
+menuFadeBg.onclick = function(event){
+     // Delete and Write menu classes
+     menu.className = "";
+     menu.className = "menu-mobile";
+     // Delete and Write fade screen classes
+     menuFadeBg.className = "";
+     menuFadeBg.className = "menu-mobile-fade-screen";
+     event.preventDefault();
+}
+
+// --------------------------------------------------------------
+// ACTIVE MENU ITEM
+// --------------------------------------------------------------
+// This solution is temporal
+// This function is very slow "thanks" to the native onload method
+
+window.onload = function() {
+    var menulinks = document.getElementById("menu-main").getElementsByTagName("a");
+    var i = 0 ;
+    var length = menulinks.length ;
+    var fullpath = location.href.split("#")[0];
+
+    // Loop through each link
+    for(; i < length; i++) {
+        if(menulinks[i].href.split("#")[0] == fullpath) {
+            menulinks[i].className += " active";
+        }
+    }
+}
+
+// --------------------------------------------------------------
+// PREVENT DEFAULT for href="#" links
+// --------------------------------------------------------------
+// This solution is temporal
+
+var nullAnchors = document.querySelectorAll('a[href="#"]');
+
+for (var i = 0; i < nullAnchors.length; i++) {
+    nullAnchors[i].addEventListener("click", preventDefaultEvent);
+}
+
+function preventDefaultEvent(e) {
     e.preventDefault();
-  });
-});
-
-$(window).resize(function(){
-   console.log('resize called');
-   var width = $(window).width();
-   if(width <= 768){
-       $('#menu-main').removeClass('menu').addClass('menu-mobile');
-   }
-   else{
-       $('#menu-main').removeClass('menu-mobile').addClass('menu');
-   }
-})
-.resize();
-
-// Active item
-// Based on: https://paulund.co.uk/use-jquery-to-highlight-active-menu-item
-
-var url = window.location.href;
-
-$('.menu li a[href="'+ url +'"]').addClass('active');
-
-$('.menu li a, .menu-mobile li a').filter(function() {
-    return this.href == url;
-}).addClass('active');
+}
